6277c25445c86d17a7acd648ef108c3d7954c2cc
minuk07 <55822543+minuk07@users.noreply.github.com>
2025-08-20 14:50:44 +0900

    [level 2] Title: 땅따먹기, Time: 21.63 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,28 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        int[][] dp = new int[land.length][4];
+        
+        for(int i=0; i<4; i++){
+            dp[0][i] = land[0][i];
+        }
+        
+        for(int i=1; i<land.length; i++){
+            for(int j=0; j<4; j++){
+                int max = 0;
+                for(int k=0; k<4; k++){
+                    if(k == j) continue;
+                    if(dp[i-1][k] > max) max = dp[i-1][k];
+                }
+                dp[i][j] = land[i][j] + max;
+            }
+        }
+        
+        for(int i=0; i<4; i++){
+            if(answer < dp[dp.length-1][i]) answer = dp[dp.length-1][i];
+        }
+
+        return answer;
+    }
+}
