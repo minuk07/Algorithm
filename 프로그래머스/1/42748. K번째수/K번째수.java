9f529afee9582e09b807a44f794f59a992cfcc13
minuk07 <55822543+minuk07@users.noreply.github.com>
2026-02-10 16:27:28 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+    
+        int[] answer = new int[commands.length];
+        
+        for(int t=0; t<commands.length; t++){
+            int i = commands[t][0]; int j = commands[t][1]; int k = commands[t][2];
+            int[] temp = new int[j-i+1];
+            
+            for(int n = 0; n <j-i+1; n++){
+                temp[n] = array[n+i-1];
+            }
+            
+            Arrays.sort(temp);
+            
+            answer[t] = temp[k-1];
+        }
+        
+        return answer;
+    }
+}
