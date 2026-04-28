436fb6f411aff0ac761f59965d704c0326d6ec62
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-04-28 14:02:04 +0900

    [level 3] Title: 거스름돈, Time: 14.53 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int len = money.length;
+        
+        int[] dp = new int[n+1];
+        
+        dp[0] =1;
+        
+        for(int m : money){
+            for(int i=m; i<=n; i++){
+                dp[i] = (dp[i] + dp[i-m]) % 1000000007;
+            }
+        }
+        
+        
+        return dp[n];
+    }
+}
