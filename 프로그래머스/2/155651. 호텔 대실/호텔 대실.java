489b15594b8ac5cfbc9c8986cf5187f86d889500
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-09-02 10:36:06 +0900

    [level 2] Title: 호텔 대실, Time: 15.87 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,32 +1,61 @@
 import java.util.*;
 
 class Solution {
+    
+    static class Hotel{
+        int start, end;
+        
+        Hotel(int start, int end){
+            this.start = start; this.end = end;
+        }
+    }
+    
     public int solution(String[][] book_time) {
+        int answer = 0;
+        
         int[][] book = new int[book_time.length][2];
         
-        for(int i=0; i<book_time.length; i++){
-            book[i][0] = Integer.parseInt(book_time[i][0].replace(":",""));
-            book[i][1] = Integer.parseInt(book_time[i][1].replace(":", "")) + 10;
+        int idx = 0;
+        
+        for(String[] b : book_time){
+            int start = Integer.parseInt(b[0].replace(":", ""));
+            int end = Integer.parseInt(b[1].replace(":","")) + 10;
             
-            if(book[i][1] % 100 >= 60) book[i][1] += 40;
-        }
-        
-        Arrays.sort(book, (a,b) -> Integer.compare(a[0], b[0]));
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        
-        for(int[] b : book){
-            if(pq.isEmpty()) pq.add(b);
-            else{
-                int tmp[] = pq.peek();
-                
-                int end = tmp[1];
-                if(b[0] >= end) pq.poll();
-                pq.add(b);
-                
+            if(end % 100 >= 60){
+                end -= 60;
+                end += 100;
             }
+            
+            book[idx][0] = start; book[idx][1] = end;
+            idx++;
         }
-                                                                
-        return pq.size();
+        
+        Arrays.sort(book, (a, b) -> a[0] - b[0]);
+        
+        PriorityQueue<Hotel> pq = new PriorityQueue<>((a,b) -> a.end - b.end);
+        
+        pq.add(new Hotel(book[0][0], book[0][1]));
+        answer++;
+        
+        idx = 1;
+        
+        while(!pq.isEmpty() && idx <= book.length -1){
+            
+            Hotel cur = pq.peek();
+            int[] next = book[idx];
+            
+            if(cur.end > next[0]){
+                answer++;
+                System.out.println(cur.end);
+                pq.add(new Hotel(next[0], next[1]));
+            }else{
+                pq.remove();
+                pq.add(new Hotel(next[0], next[1]));
+            }
+            
+            idx++;
+        }
+        
+        return answer;
     }
 }
