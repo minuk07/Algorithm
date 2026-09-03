57f6eb1d7fe714c05efca01ff5e37669b6993987
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-09-03 11:01:57 +0900

    [level 3] Title: 섬 연결하기, Time: 1.40 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,11 +4,11 @@ class Solution {
     
     static int[] root;
     
-    static int find(int a){
-        if(root[a] == a) return a;
+    static int find(int idx){
+        if(root[idx] == idx) return idx;
         
-        int r = find(root[a]);
-        root[a] = r;
+        int r = find(root[idx]);
+        root[idx] = r;
         return r;
     }
     
@@ -28,16 +28,17 @@ class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        Arrays.sort(costs, (a,b) -> a[2] - b[2]);
         root = new int[n];
         
         for(int i=0; i<n; i++){
             root[i] = i;
         }
         
-        for(int i=0; i<costs.length; i++){
-            if(union(costs[i][0], costs[i][1])){
-                answer += costs[i][2];
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+        
+        for(int[] cost : costs){
+            if(union(cost[0], cost[1])){
+                answer += cost[2];
             }
         }
         
