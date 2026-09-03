c8c6c0035e2555f3239539cccd644573da86580e
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-09-03 12:22:23 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.69 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -5,16 +5,18 @@ class Solution {
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
     
-    static int sy, sx;
     static int answer;
     
     static int n, m;
-    static char[][] map;
+    static int[][] map;
+    static int startY, startX;
+    static int destY, destX;
     
-    static class Node{
-        int r,c;
-        Node(int r, int c){
-            this.r = r; this.c = c;
+    static class Robot{
+        int r, c, cnt;
+        
+        Robot(int r, int c,int cnt){
+            this.r = r; this.c = c; this.cnt = cnt;
         }
     }
     
@@ -22,62 +24,75 @@ class Solution {
         return (y>=0 && x>=0 && y<n && x<m);
     }
     
-    public int solution(String[] board) {
+    static void bfs(Robot robot){
         
-        n = board.length;
-        m = board[0].length();
-        
-        map = new char[n][m];
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                map[i][j] = board[i].charAt(j);
-                if(map[i][j] == 'R'){
-                    sy = i; sx =j;
-                }
-            }
-        }
-        
-        answer = -1;
-        
-        bfs(sy, sx);
-        
-        return answer;
-    }
-    
-    static void bfs(int y, int x){
-        Queue<Node> q = new LinkedList<>();
-        int[][] dist = new int[n][m];
         boolean[][] visited = new boolean[n][m];
-        visited[y][x] = true;
-        q.add(new Node(y, x));
+        Queue<Robot> q = new LinkedList<>();
+        q.add(robot);
+        visited[robot.r][robot.c] = true;
         
         while(!q.isEmpty()){
-            Node cur = q.poll();
-            int cy = cur.r;
-            int cx = cur.c;
+            Robot cur = q.poll();
             
-            if(map[cy][cx] == 'G'){
-                answer = dist[cy][cx];
+            int y = cur.r; int x = cur.c; int cnt = cur.cnt;
+            
+            if(y == destY && x == destX){
+                answer = cnt;
                 break;
             }
             
             for(int i=0; i<4; i++){
-                int ddy = cy + dy[i];
-                int ddx = cx + dx[i];
-                
-                while(inRange(ddy, ddx) && map[ddy][ddx] != 'D'){
-                    ddy = ddy + dy[i];
-                    ddx = ddx + dx[i];
+            
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                while(inRange(ny, nx) && map[ny][nx] == 0){
+                    ny += dy[i];
+                    nx += dx[i];
                 }
-                ddy -= dy[i];
-                ddx -= dx[i];
-                
-                if(visited[ddy][ddx]) continue;
-                visited[ddy][ddx] = true;
-                dist[ddy][ddx] = dist[cy][cx] + 1;
-                q.add(new Node(ddy,ddx));
+
+                ny -= dy[i];
+                nx -= dx[i];
+
+                if(visited[ny][nx]) continue;
+
+                visited[ny][nx] = true;
+                q.add(new Robot(ny, nx, cnt + 1));
             }
         }
+        
+        
+        
+        return;
+    }
+    
+    public int solution(String[] board) {
+        answer = 0;
+        
+        n = board.length;
+        m = board[0].length();
+        map = new int[n][m];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                char tmp = board[i].charAt(j);
+                
+                if(tmp == 'R'){
+                    startY = i; startX = j;
+                }else if(tmp == 'G'){
+                    destY = i; destX = j;
+                }else if(tmp == 'D'){
+                    map[i][j] = -1;
+                }else{
+                    map[i][j] = 0;
+                }
+            }
+        }
+        
+        bfs(new Robot(startY, startX, 0));
+        
+        if(answer != 0) return answer;
+        
+        return -1;
     }
 }
