6e38a991025ba154fad38866f54dce5c8ff56fd7
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-09-03 10:38:37 +0900

    [level 2] Title: 디펜스 게임, Time: 62.72 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -3,23 +3,32 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(b, a));
         
-        for(int i=0; i<enemy.length; i++){
-            int e = enemy[i];
-            pq.add(e);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        
+        int sum = 0;
+        
+        for(int round = 0; round<enemy.length; round++){
+            int cur = enemy[round];
             
-            n -= e;
-            if(n < 0){
-                if(k > 0){
-                    k--;
-                    n += pq.poll();
-                }
-                else{
-                    break;
-                }
+            pq.add(cur);
+            sum += cur;
+            
+            if(sum > n && k > 0){
+                k--;
+                sum -= pq.poll();
             }
-            answer++;
+            
+            if(sum > n){
+                answer = round;
+                break;
+            }
+            
+            if(round == enemy.length - 1){
+                answer = enemy.length;
+            }
+            
+            
         }
         
         return answer;
