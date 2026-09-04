69fe869c15f3a2d9c390bfb3badd8bce8c8ff4ff
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-09-04 11:49:10 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -3,25 +3,33 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
-        List<Integer> list = new ArrayList<>();
         
-        long t = 1;
+        long[] fact = new long[n+1];
+        
+        fact[0] = 1;
+        
+        for(int i=1; i<=n; i++){
+            fact[i] = fact[i-1] * i;
+        }
+        
+        List<Integer> list = new ArrayList<>();
         
         for(int i=1; i<=n; i++){
             list.add(i);
-            t *= i;
         }
         
         k--;
         
-        int idx = 0;
-        
-        while(idx < n){
-            t /= n-idx;
-            answer[idx] = list.remove((int) (k/t));
-            idx++;
-            k %= t;
+        for(int i=0; i<n; i++){
+            long tmp = fact[n-1-i];
+            int nowIdx = (int)(k / tmp);
+            
+            answer[i] = list.get(nowIdx);
+            list.remove(list.get(nowIdx));
+            
+            k %= tmp;
         }
+        
         
         return answer;
     }
