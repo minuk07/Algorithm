9f036e19cfcdc03f44252120293ec8b2ecbdd728
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-09-05 20:20:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.72 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,32 +2,25 @@ import java.util.*;
 
 class Solution {
     
-    static class Disk{
+    static class Job{
+        
         int idx, start, take;
         
-        Disk(int idx, int start, int take){
+        Job(int idx, int start, int take){
             this.idx = idx; this.start = start; this.take = take;
         }
     }
     
     public int solution(int[][] jobs) {
         int answer = 0;
-        int sum = 0;
         
-        int[][] disks = new int[jobs.length][3];
+        Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
         
-        for(int i=0; i<jobs.length; i++){
-            disks[i][0] = i;
-            disks[i][1] = jobs[i][0];
-            disks[i][2] = jobs[i][1];
-        }
-        
-        Arrays.sort(disks, (a,b) -> a[1] - b[1]);
-        
-        PriorityQueue<Disk> pq = new PriorityQueue<>((a,b) -> {
+        PriorityQueue<Job> pq = new PriorityQueue<>((a,b) -> {
             if(a.take != b.take){
                 return a.take - b.take;
             }
+            
             if(a.start != b.start){
                 return a.start - b.start;
             }
@@ -35,33 +28,28 @@ class Solution {
             return a.idx - b.idx;
         });
         
+        int cnt = 0;
         int idx = 0;
         int time = 0;
-        int count = 0;
         
-        while(count < disks.length){
+        while(cnt < jobs.length){
             
-            while(idx < disks.length && disks[idx][1] <= time){
-                pq.add(new Disk(disks[idx][0], disks[idx][1], disks[idx][2]));
+            while(idx < jobs.length && time >= jobs[idx][0]){
+                pq.add(new Job(idx, jobs[idx][0], jobs[idx][1]));
                 idx++;
             }
             
-            if(!pq.isEmpty()){
-                Disk cur = pq.poll();
+            if(pq.isEmpty()){
+                time++;
+            }else{
+                Job cur = pq.poll();
                 
                 time += cur.take;
-                
-                sum += (time - cur.start);
-                
-                count++;
-            }
-            else{
-                time = disks[idx][1];
+                answer += (time - cur.start);
+                cnt++;
             }
         }
         
-        answer = sum / jobs.length ;
-        
-        return answer;
+        return answer / jobs.length;
     }
 }
