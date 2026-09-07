0332de48068979a44f1fcabc704acec49797618e
Lee Minwook <55822543+minuk07@users.noreply.github.com>
2026-09-07 12:17:10 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 27.60 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,52 +1,57 @@
 class Solution {
     
-    static int n, m;
-    static int[][] temp;
+    static int[][] sum;
     
-    static void attckOrShield(int type, int r1, int c1, int r2, int c2, int degree){
-        if(type == 1){
-            temp[r1][c1] -= degree; temp[r1][c2+1] += degree;
-            temp[r2+1][c1] += degree; temp[r2+1][c2+1] -= degree;
-        }
-        else{
-            temp[r1][c1] += degree; temp[r1][c2+1] -= degree;
-            temp[r2+1][c1] -= degree; temp[r2+1][c2+1] += degree;
-        }
-    }
-    
-    static void sumArray(){
-        for(int i=0; i<n; i++){
-            for(int j=1; j<m; j++){
-                temp[i][j] += temp[i][j-1];
-            }
-        }
-        
-        for(int j=0; j<=m; j++){
-            for(int i=1; i<=n; i++){
-                temp[i][j] += temp[i-1][j];
-            }
+    static void destroy(int type, int r1, int c1, int r2, int c2, int degree){
+        if(type == 1){ //공격
+            sum[r1][c1] -= degree;
+            sum[r1][c2+1] += degree;
+            sum[r2+1][c1] += degree;
+            sum[r2+1][c2+1] -= degree;
+        }else{
+            sum[r1][c1] += degree;
+            sum[r1][c2+1] -= degree;
+            sum[r2+1][c1] -= degree;
+            sum[r2+1][c2+1] += degree;
         }
     }
     
     public int solution(int[][] board, int[][] skill) {
-        int answer = 0; 
-        n = board.length;
-        m = board[0].length;
+        int answer = 0;
         
-        temp = new int[n+1][m+1];
+        int n = board.length;
+        int m = board[0].length;
+        
+        sum = new int[n+1][m+1];
         
         for(int[] s : skill){
-            attckOrShield(s[0], s[1], s[2], s[3], s[4], s[5]);
+            destroy(s[0], s[1], s[2], s[3], s[4], s[5]);
         }
         
-        sumArray();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                sum[i][j] += sum[i][j - 1];
+            }
+        }
+
+        for (int j = 0; j <= m; j++) {
+            for (int i = 1; i <= n; i++) {
+                sum[i][j] += sum[i - 1][j];
+            }
+        }
+            
+        // for(int i=0; i<=n; i++){
+        //     for(int j=0; j<=m; j++){
+        //         System.out.print(result[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(board[i][j] + temp[i][j] > 0) answer++;
+                if(board[i][j] + sum[i][j] > 0) answer++;
             }
         }
-        
         
         return answer;
     }
